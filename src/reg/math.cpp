89d521e1e05712658c2cb1ba1e6e389ff18b0c47
shlockie-np <shlockie.np@gmail.com>
2026-04-22 01:42:01 -0500

    Update math.cpp

--- src/reg/math.cpp
+++ src/reg/math.cpp
@@ -1,5 +1,5 @@
 /**
-* @file standard_math.cpp
+* @file math.cpp
  * @brief Defines and loads all default mathematical operations, functions, and constants.
  * @details This file acts as the standard library for the CLI calculator.
  * It isolates all the actual computational logic (using `<cmath>`) from the parsing
