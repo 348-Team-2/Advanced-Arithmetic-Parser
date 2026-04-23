74ea7b0a117ea3d82abb5065b14c4a4c1090381a
Emrillion <129785985+Emrillion@users.noreply.github.com>
2026-04-23 15:57:04 -0500

    Add CLI class

---
+++ src/cli/cli.cpp
@@ -0,0 +1,26 @@
+/**
+* @file cli.cpp
+ * @brief Manages all user input and output from the CLI.
+ * @details This file is responsible for reading user input and printing results or error messages.
+ */
+
+#include "cli/cli.hpp"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+void CLI::print(const std::string& message) {
+    std::cout << message << std::endl;
+}
+
+// reads input and removes all whitespace characters
+std::string CLI::read_input() {
+    std::string input;
+    std::getline(std::cin, input);
+    input.erase(std::remove_if(input.begin(), input.end(), ::isspace), input.end());
+    
+    // add "\n" to end input
+    input += "\n";
+    return input;
+}
